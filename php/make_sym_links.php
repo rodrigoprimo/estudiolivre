7dfc9fca5b88aa95f8ef2a2329be1ae4f4606bc9
lfagundes <lfagundes>
2006-05-09 14:34:08 +0000

    arrumando symlinks pra todos os temas das categorias funcionarem

---
+++ php/make_sym_links.php
@@ -0,0 +1,7 @@
+<?php
+
+  system("ln -s estudiolivre templates/styles/estudiolivre_audio");
+  system("ln -s estudiolivre templates/styles/estudiolivre_video");
+  system("ln -s estudiolivre templates/styles/estudiolivre_grafico");
+
+?>
