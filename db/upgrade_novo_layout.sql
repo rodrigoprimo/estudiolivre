d57504dc525e3e7d244c26ee4396e25d5b63a05c
lfagundes <lfagundes>
2006-05-09 18:01:52 +0000

    refazendo o lance das categs no banco, pra nao precisar mais de links e tal.

---
+++ db/upgrade_novo_layout.sql
@@ -0,0 +1,1 @@
+delete from tiki_theme_control_categs;
--- php/make_sym_links.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-  system("ln -s estudiolivre templates/styles/estudiolivre_audio");
-  system("ln -s estudiolivre templates/styles/estudiolivre_video");
-  system("ln -s estudiolivre templates/styles/estudiolivre_grafico");
-
-?>
