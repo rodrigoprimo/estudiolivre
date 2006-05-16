727e01f0956e904fe0714731b87d50741346be38
rhwinter <rhwinter>
2006-05-16 13:23:07 +0000

    [NEW] automatização dos modulos

---
+++ db/modules.sql
@@ -0,0 +1,1 @@
+INSERT INTO `tiki_modules` VALUES ('last_modif_pages','l',1,NULL,'',0,10,'','a:1:{i:0;s:10:\"Registered\";}'),('who_is_there','l',2,NULL,'',0,10,'','a:1:{i:0;s:10:\"Registered\";}');
