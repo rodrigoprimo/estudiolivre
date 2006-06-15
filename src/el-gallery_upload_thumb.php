4f952061328e636d253d57ab849006cf291ef429
lfagundes <lfagundes>
2006-06-15 18:15:11 +0000

    [FIX] upload do thumb nao estava funcioando

--- src/el-gallery_upload_thumb.php
+++ src/el-gallery_upload_thumb.php
@@ -20,26 +20,39 @@ if ($arquivoId && isset($_FILES['thumb']) && !empty($_FILES['thumb']['name'])) {
 	
     }
 
+    /*    ob_start();
+    print_r($_FILES);
+    $saida = ob_get_contents();
+    ob_end_clean();
+    error($saida);*/
+
     // Were there any problems with the upload?  If so, report here.
     if (!is_uploaded_file($_FILES["thumb"]['tmp_name'])) {
 	error(tra('Upload was not successful').': '.ELGalLib::convert_error_to_string($_FILES["thumb"]['error']));
     } 
     
     $maxSize = $tikilib->get_preference('el_max_thumb_size', 200);
-    
+
     if ($_FILES['thumb']["size"] > $maxSize * 1024) {
 	error("O tamanho máximo da miniatura é de $maxSize kBytes.");
     }
     
+    if (preg_match('/(\..+?)$/', $_FILES["thumb"]["name"], $m)) {
+	$ext = $m[1];
+    } else {
+	$ext = ".png";
+    } 
+
     $handle = fopen($_FILES['thumb']['tmp_name'], "r");
     $thumbData = fread($handle, $_FILES['thumb']['size']);
     fclose($handle);
     
     global $userlib;
     $userId = $userlib->get_user_id($user);
-    $error = $elgallib->save_thumb($thumbData,$arquivoId,$userId);
-    if ($error) {
-	error(tra('Upload was not successful').': '.$error);
+    
+    $result = $elgallib->save_thumb($thumbData, $arquivoId, $userId, $ext);
+    if (!$result) {
+	error(tra('Impossivel gravar miniatura'));
     }
     
     $arquivo = $elgallib->get_arquivo($arquivoId);
