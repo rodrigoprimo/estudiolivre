ca9c3409fa6c1612cf98876ddfadd1eb39f18e8c
lfagundes <lfagundes>
2006-06-19 16:18:10 +0000

    [FIX] Se o tooltip nao tiver nome, pega o md5 do tooltip
    asa e milho

--- src/lib/smarty_tiki/block.tooltip.php
+++ src/lib/smarty_tiki/block.tooltip.php
@@ -21,9 +21,13 @@ function smarty_block_tooltip($params, $text) {
  	
  	global $tooltiplib, $feature_tooltip, $feature_tooltip_max_clicks;
  	
+	if (!$tipName) {
+	    $tipName = md5($tip);
+	}
+
  	$clicks = $tooltiplib->get_user_clicks($tipName);
  	
- 	if ($feature_tooltip == 'y' && ($clicks <= $feature_tooltip_max_clicks || !$feature_tooltip_max_clicks)) {
+       	if ($feature_tooltip == 'y' && ($clicks <= $feature_tooltip_max_clicks || !$feature_tooltip_max_clicks)) {
  		$text = "<span onMouseover=\"tooltip('$tip');\" onMouseout=\"nd();\" onMousedown=\"xajax_register_tooltip_click('$tipName');\">".$text."</span>";
  	}
  	
