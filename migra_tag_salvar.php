825b3512aaa3660c623949b12432c0e793369fd3
lfagundes <lfagundes>
2006-07-07 20:01:37 +0000

    script de migracao, ja deve estar rolando, mas ainda preciso reproduzir os dois ambientes pra poder testar

--- migra_tag_salvar.php
+++ migra_tag_salvar.php
@@ -1,14 +1,102 @@
 <?
 
-require_once("tiki-setup.php");
-
-if (basename(__FILE__) !== '/noe/data/vhost/teste.estudiolivre.org/htdocs/migra_tag_salvar.php') {
+if (basename(__FILE__) !== '/noe/data/vhost/estudiolivre.org/htdocs/migra_tag_salvar.php') {
     echo "tentando rodar no lugar errado";
     exit;
 }
 
+require_once("tiki-setup.php");
+require_once("lib/freetag/freetaglib.php");
+
 $tagId = 35; // id da tag salvar
 
-$query = "select tko.itemId, tko.type from tiki_freetagged_objects tfo, tiki_objects tko where tko.objectId=tfo.objectId and tfo.tagId=?";
+$query = "select tko.itemId, tko.type, tko.objectId from estudiolivre_teste.tiki_freetagged_objects tfo, estudiolivre_teste.tiki_objects tko where tko.objectId=tfo.objectId and tfo.tagId=? order by tko.type";
+
+$result = $tikilib->query($query, array($tagId));
+
+while ($row = $result->fetchRow()) {
+    $itemId = $row['itemId'];
+    $type = $row['type'];
+    $objId = $row['objectId'];
+
+    if ($type == 'gallery') {
+	migra_acervo($itemId, $objId);
+    } elseif ($type == 'wiki page') {
+	migra_wiki($itemId, $objId);
+    }
+}
+
+function migra_wiki($pageName, $objId_from) {
+    global $tikilib, $freetaglib;
+
+    $query = "select * from estudiolivre_teste.tiki_pages where pageName=?";
+    $result = $tikilib->query($query, array($pageName));
+    $page = $result->fetchRow();
+
+    if ($tikilib->page_exists($pageName)) {
+	$tikilib->update_page($pageName, 
+			      $page['data'],
+			      $page['comment'],
+			      $page['user'],
+			      $page['ip'],
+			      $page['description']);
+    } else {
+	$tikilib->create_page($pageName,
+			      $page['hits'],
+			      $page['data'],
+			      $page['lastModif'],
+			      $page['comment'],
+			      $page['user'],
+			      $page['ip'],
+			      $page['description']);
+    }
+
+    $cat_type='wiki page';
+    $cat_desc = substr($page["description"],0,200);
+    $cat_name = $pageName;
+    $cat_href="tiki-index.php?page=".urlencode($pageName);
+
+    $freetaglib->add_object($cat_type, $pageName, $cat_desc, $cat_name, $cat_href);
+
+    $query = "select tf.tag as tag from estudiolivre_teste.tiki_freetagged_objects tfo, estudiolivre_teste.tiki_freetags tf where tfo.tagId=tf.tagId and tfo.objectId=?";
+    $result = $tikilib->query($query, array($objId_from));
+    while ($row = $result->fetchRow()) {
+	$freetaglib->safe_tag($page['user'], $pageName, $cat_type, $row['tag']);
+    }
+}
+
+function migra_acervo($arquivoId, $objId_from) {
+    global $tikilib;
+
+    $bindvals = array($arquivoId);
+
+    $query = "select * from estudiolivre_teste.el_arquivo where arquivoId=?";
+    $result = $tikilib->query($query, $bindvals);
+    $arquivo = $result->fetchRow();
+
+    $tipo = strtolower($arquivo['tipo']);
+
+    $query = "insert into el_arquivo select * from estudiolivre_teste.el_arquivo where arquivoId=?";
+    $tikilib->query($query, $bindvals);
+
+    $query = "insert into el_arquivo_$tipo select * from estudiolivre_teste.el_arquivo_$tipo where arquivoId=?";
+    $tikilib->query($query, $bindvals);
+    
+    $query = "insert into el_arquivo_rating select * from estudiolivre_teste.el_arquivo_rating where arquivoId=?";
+    $tikilib->query($query, $bindvals);
+
+    $query = "insert into el_arquivo_$tipo select * from estudiolivre_teste.el_arquivo_$tipo where arquivoId=?";
+    $tikilib->query($query, $bindvals);
+    
+    $href = "el-gallery_view.php?arquivoId=$arquivoId";
+    $freetaglib->add_object('gallery', $arquivoId, $arquivo['descricao'], $arquivo['titulo'], $href);
+
+    $query = "select tf.tag as tag from estudiolivre_teste.tiki_freetagged_objects tfo, estudiolivre_teste.tiki_freetags tf where tfo.tagId=tf.tagId and tfo.objectId=?";
+    $result = $tikilib->query($query, array($objId_from));
+    while ($row = $result->fetchRow()) {
+	$freetaglib->safe_tag($arquivo['user'], $arquivoId, 'gallery', $row['tag']);
+    }
+    
+}
 
 ?>
