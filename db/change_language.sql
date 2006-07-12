ad01f6dda5811bd58bc6344c86daad9e64b76812
lfagundes <lfagundes>
2006-07-12 15:53:58 +0000

    detecta idioma do browser limita idiomas disponiveis pro usuario: portugues, ingles e espanhol

--- db/change_language.sql
+++ db/change_language.sql
@@ -1,2 +1,4 @@
 REPLACE INTO tiki_preferences (name,value) VALUES ('change_language','y');
 REPLACE INTO tiki_preferences (name,value) VALUES ('feature_best_language','y');
+REPLACE INTO tiki_preferences (name,value) VALUES ('feature_detect_language','y');
+REPLACE INTO tiki_preferences (name,value) VALUES ('available_languages','a:3:{i:0;s:2:"es";i:1;s:2:"en";i:2;s:5:"pt-br";}');
