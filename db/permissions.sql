0d58a31edfe91da7b2f6da701f172950d54b4f3e
nano <nano>
2006-07-12 16:19:42 +0000

    oops, tinha me deixado no grupo por teste, mas n?o quero ser moderador :P

--- db/permissions.sql
+++ db/permissions.sql
@@ -15,4 +15,4 @@ insert into users_grouppermissions values ('Registered','tiki_p_vote_poll','y');
 delete from users_grouppermissions where permName = 'el_p_admin_gallery';
 insert into users_groups values('Moderadores do acervo', 'Colaboradores na moderação dos arquivos enviados ao acervo', '', 'n', null, null, null, null);
 insert into users_grouppermissions values ('Moderadores do acervo', 'el_p_admin_gallery', 'y');
-insert into users_usergroups values (4, 'Moderadores do acervo'), (24, 'Moderadores do acervo'), (36, 'Moderadores do acervo'), (153, 'Moderadores do acervo'), (393, 'Moderadores do acervo'), (678, 'Moderadores do acervo');
+insert into users_usergroups values (4, 'Moderadores do acervo'), (24, 'Moderadores do acervo'), (36, 'Moderadores do acervo'), (153, 'Moderadores do acervo'), (678, 'Moderadores do acervo');
