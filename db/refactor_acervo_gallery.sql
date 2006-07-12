a3831580300e9a814bad40794fadc7aefb5d8b23
nano <nano>
2006-07-12 21:47:01 +0000

    tirando sql que nao rpecisa mais

--- db/refactor_acervo_gallery.sql
+++
@@ -1,2 +0,0 @@
-update tiki_objects set type = 'gallery' where type = 'acervo';
-update tiki_objects set href = concat('el-gallery_view.php',substring(href, 15)) where href regexp 'el-arquivo\.php';
