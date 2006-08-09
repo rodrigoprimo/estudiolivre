f2efe8ca92cd11bc5411017f62bcc79d0e9e8686
nano <nano>
2006-08-09 16:15:36 +0000

    [NEW] agora acervotag (tem que mudar esse nome) funciona por id tb!

--- src/lib/wiki-plugins/wikiplugin_acervotag.php
+++ src/lib/wiki-plugins/wikiplugin_acervotag.php
@@ -1,32 +1,49 @@
 <?php
 
 function wikiplugin_acervotag_help() {
-    $help = tra("Mostra lista de arquivos do acervo com determinada tag") . "\n";
-    $help.= "~np~{ACERVOTAG(tag=>MinhaTag)}{ACERVOTAG}~/np~";
+    $help = tra("Mostra lista de arquivos do acervo com determinada tag ou por id") . "<br/>";
+    $help.= "~np~{ACERVOTAG(tag=>MinhaTag)}{ACERVOTAG}~/np~" . "<br/>";
+    $help.= "~np~{ACERVOTAG(id=>idDoArquivo)}{ACERVOTAG}~/np~";
     return $help;
 }
+
+function retrieveFileInfo($id) {
+	
+	global $freetaglib, $elgallib, $commentslib;
+	
+	require_once("lib/elgal/elgallib.php");
+		
+	$arquivo = $elgallib->get_arquivo($id);
+	$arquivo['commentsCount'] = $commentslib->count_comments('arquivo:' . $id);
+	$arquivo['tags'] = $freetaglib->get_tags_on_object($id, 'gallery');
+	$arquivo['descricaoLicenca'] = $arquivo['licenca']['descricao'];
+	$arquivo['linkImagem'] = $arquivo['licenca']['linkImagem'];
+	$arquivo['linkHumanReadable'] = $arquivo['licenca']['linkHumanReadable'];
+	
+	return $arquivo;
+}
+
 function wikiplugin_acervotag($data, $params) {
 
-    global $freetaglib, $elgallib, $smarty, $commentslib;
-
+    global $smarty, $freetaglib; 
+    
     require_once("lib/freetag/freetaglib.php");
-    require_once("lib/elgal/elgallib.php");
+        
+    if(isset($params['id']) && $params['id'] > 0) {
+    	$result = "";
+		$smarty->assign_by_ref("arquivo", retrieveFileInfo($params['id']));
+		$result .= $smarty->fetch('el-gallery_list_item.tpl');
+	    return "~np~$result~/np~";
+    }
     
     $objects = $freetaglib->get_objects_with_tag($params['tag'], 'gallery');
     $result = "";
     
     foreach ($objects['data'] as $object) {
-	$arquivo = $elgallib->get_arquivo($object['itemId']);
-	$arquivo['commentsCount'] = $commentslib->count_comments('arquivo:' . $object['itemId']);
-	$arquivo['tags'] = $freetaglib->get_tags_on_object($object['itemId'], 'gallery');
-	$arquivo['descricaoLicenca'] = $arquivo['licenca']['descricao'];
-	$arquivo['linkImagem'] = $arquivo['licenca']['linkImagem'];
-	$arquivo['linkHumanReadable'] = $arquivo['licenca']['linkHumanReadable'];
-
-	$smarty->assign_by_ref("arquivo", $arquivo);
-
-	$result .= $smarty->fetch('el-gallery_list_item.tpl');
+		$smarty->assign_by_ref("arquivo", retrieveFileInfo($object['itemId']));
+		$result .= $smarty->fetch('el-gallery_list_item.tpl');
     }
+
     return "~np~$result~/np~";
     
 }
