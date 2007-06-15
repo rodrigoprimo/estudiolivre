35a33b28db17fa7c11fc0b2ce582131b6ae5a400
nano <nano>
2007-06-15 23:00:01 +0000

    [FIX] download te redireciona pro arquivo, assim possibilitando resume nos browser e wgets da vida...

--- src/el-download.php
+++ src/el-download.php
@@ -11,20 +11,10 @@ if (!isset($_REQUEST['arquivo']) || $_REQUEST['arquivo'] == 0) {
 
 $arquivo = PersistentObjectFactory::createObject("Publication", (int)$_REQUEST['arquivo']);
 
-header('Content-Description: File Transfer');
-header('Content-Type: application/force-download');
 $file =& $arquivo->filereferences[0];
+$file->hitDownload();
 
-if(isset($_REQUEST['action']) and $_REQUEST['action'] == 'download') {
-    header("Content-Disposition: attachment; filename=\"$file->parseDownloadName()\"");
-    $file->hitDownload();
-}
-
-$fileinfo = stat($file->fullPath());
-header("Content-length: ".$fileinfo[7]);
-
-session_write_close();
-readfile($file->fullPath());
+header("location: " . $file->fullPath());
 exit;
 
 ?>
