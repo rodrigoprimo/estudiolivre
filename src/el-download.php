3881c91bedc63780757535b8554bc74e60b13cbd
uid13421 <uid13421>
2009-03-19 15:47:33 +0000

    [FIX] força download de arquivos mp3

--- src/el-download.php
+++ src/el-download.php
@@ -21,11 +21,13 @@ if (isset($_REQUEST['file'])) {
 	$file =& $pub->filereferences[(int)$_REQUEST['file']];
 	$file->hitDownload();
 	$location = $file->fullPath();
+    if ($file->mimeType == 'audio/mpeg') addMp3Headers($location, $file->size); //para forçar o download
 } elseif (isset($_REQUEST['action']) && $_REQUEST['action'] == 'downloadAll') {
 	if (count($pub->filereferences) == 1) {
 		$file =& $pub->filereferences[0];
 		$file->hitDownload();
 		$location = $file->fullPath();
+        if ($file->mimeType == 'audio/mpeg') addMp3Headers($location, $file->size); //para forçar o download
 	} else {
 		$fileList = "";
 		foreach ($pub->filereferences as $file) {
@@ -38,6 +40,23 @@ if (isset($_REQUEST['file'])) {
 		}
 		$location = $pub->allFile;
 	}
+}
+
+function addMp3Headers($filename, $size) {
+   // required for IE, otherwise Content-disposition is ignored
+       if(ini_get('zlib.output_compression')) { ini_set('zlib.output_compression', 'Off'); }
+       
+       // build file headers
+   header("Pragma: public");
+   header("Expires: 0");
+   header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+   header("Cache-Control: private",false);
+   header("Content-Type: audio/x-mp3");
+   header("Content-Disposition: attachment; filename=\"$filename\";" );
+   header("Content-Transfer-Encoding: binary");
+   header("Content-Length: $size");
+   readfile($filename);
+   exit;
 }
 
 header("location: $location");
