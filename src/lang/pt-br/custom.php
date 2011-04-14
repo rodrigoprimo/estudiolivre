17e41096b5d022fa64046d5acae36573b143ce2a
Rodrigo Sampaio Primo <rodrigo@utopia.org.br>
2011-04-14 15:10:24 -0300

    corrige traducao da palavra tags

--- src/lang/pt-br/custom.php
+++ src/lang/pt-br/custom.php
@@ -5,7 +5,8 @@ $lang_custom = array(
  "Post new message"=>"Enviar mensagem",
  "registered users"=>"usuários cadastrados",
  "last topics in this forum"=>"ultimos topicos neste forum",
- "Add another image" => "Adicionar outra imagem"
+ "Add another image" => "Adicionar outra imagem",
+ "Tags" => "Tags",
 );
 $lang = array_merge($lang, $lang_custom);
 ?>
